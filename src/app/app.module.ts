2e160b8dd3b758f671f4bcec4cdea6e11f61bb86
ekbal <zayan.ekbal@gmail.com>
2019-01-11 00:28:14 +0200

    angular form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormioAppConfig } from 'angular-formio';
+import { FormioAuthService, FormioAuthConfig } from 'angular-formio/auth';
+import { AuthConfig, AppConfig } from '../config';
+import { AuthModule } from './auth/auth.module';
+import { EventModule } from './event/event.module';
+import { FormioResources } from 'angular-formio/resource';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'auth',
+        loadChildren: './auth/auth.module#AuthModule'
+      },
+      {
+        path: 'event',
+        loadChildren: () => EventModule
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    FormioResources,
+    FormioAuthService,
+    {provide: FormioAuthConfig, useValue: AuthConfig},
+    {provide: FormioAppConfig, useValue: AppConfig}
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
